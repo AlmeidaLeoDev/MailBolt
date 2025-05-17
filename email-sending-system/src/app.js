ce1b671709b24c50c82e826fe9c7336de376b334
Leonardo <almeidaleo.dev@gmail.com>
2025-05-16 23:21:46 -0300

    feat(server): add basic email sending functionality with Express and Nodemailer
    Set up Express server.
    
    Integrate Nodemailer for email sending.
    
    Configure EJS as the view engine.
    
    Add body-parser for parsing form data.
    
    Create basic route for sending emails.

---
+++ email-sending-system/src/app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
+const PORT = 3000;
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+// Handle email sending
+app.post('/send-email', (req, res) => {
+    const {recipient, subject, message} = req.body; // Destructuring
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'gmail',
+            pass: 'password'
+        }
+    });
+
+    // Define the email options
+    const mailOptions = {
+        from: 'sender mail', 
+        to: recipient, 
+        subject: subject, 
+        text: message,
+    };
+
+    // Send the email
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error){
+            console.error("Error occurred", error);
+            res.status(500).send('Error in sending email. Please try again later.'); } else{
+                console.log('Email sent:', info.response);
+                res.send('Email sent successfully');
+            }
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on port ${PORT}`);
+});
